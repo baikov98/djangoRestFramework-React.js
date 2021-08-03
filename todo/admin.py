2fd097d5505c421113cc98776928c3e1273104e1
Vilmir Baikov <noname777228@rambler.ru>
2021-08-03 23:53:01 +0500

    initial model

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from django.db import models
+from django.contrib import admin
 # Register your models here.
+
+
+class TodoAdmin(models.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'date', 'done')
+    list_display_links = 
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=300, blank=True)
+    date = models.DateTimeField(auto_now_add=False)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
