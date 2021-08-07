425a06208bcf2be6b6ef8aa7530df8b26fdc9eca
Vilmir Baikov <noname777228@rambler.ru>
2021-08-07 12:26:37 +0500

    imports fix

--- todo/admin.py
+++ todo/admin.py
@@ -8,7 +8,7 @@ class TodoAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'date', 'done')
     list_display_links = ('id', 'title')
     search_fields = ('id', 'title', 'description')
-    list_editable = ('done')
-    list_filter = ('done')
+    list_editable = ('done',)
+    list_filter = ('done',)
 
 admin.site.register(Todo, TodoAdmin)
--- todo/api.py
+++ todo/api.py
@@ -1,10 +1,10 @@
 from .models import Todo
-from rest_framework import viewsets, permitions
+from rest_framework import viewsets, permissions
 from .serializers import TodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
-    permitions_classes = [
-        permitions.AllowAny
+    permissions_classes = [
+        permissions.AllowAny
     ]
     seriaizer_class = TodoSerializer
